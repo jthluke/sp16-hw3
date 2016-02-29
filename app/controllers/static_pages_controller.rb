0cd13172947bbf567f6e61f6f7884b603d95d238
Justin Luke <justin.luke@berkeley.edu>
2016-02-28 16:22:31 -0800

    Finished HW3

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def home
+  	@users = User.all
+  	@cats = Cat.all
+  	@todos = Todo.all
+  end
+end
---
+++ app/models/cat.rb
@@ -0,0 +1,5 @@
+class Cat < ActiveRecord::Base
+	def meow
+		"me0W!!!11!"
+	end
+end
